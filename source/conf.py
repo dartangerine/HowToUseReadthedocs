3daa9b114dcfde0ceb2f11ff10f2a43bf6eb2b15
sunpx <spxspxlhqlhq@gmail.com>
2025-09-18 19:56:17 +0800

    change_theme

--- source/conf.py
+++ source/conf.py
@@ -17,12 +17,14 @@ release = '0.0.1'
 extensions = []
 
 templates_path = ['_templates']
-exclude_patterns = []
+extensions = ['recommonmark']
 
 
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'alabaster'
+# html_theme = 'alabaster'
+import sphinx_rtd_theme
+html_theme = "sphinx_rtd_theme"
 html_static_path = ['_static']
